01fffdd4c1da9f9a1ad6a3ff23fba15ebbec2c92
bryanwessantana <bryanwessantana@gmail.com>
2026-01-26 22:10:00 -0300

    Fix: Added try/catch blocks and removed .env from repository

--- gym-api/controllers/trainers.js
+++ gym-api/controllers/trainers.js
@@ -2,69 +2,83 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('trainers').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const result = await mongodb.getDb().db().collection('trainers').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const trainerId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('trainers').find({ _id: trainerId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const trainerId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('trainers').find({ _id: trainerId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createTrainer = async (req, res) => {
-  const trainer = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    specialty: req.body.specialty,
-    certification: req.body.certification
-  };
-  const response = await mongodb.getDb().db().collection('trainers').insertOne(trainer);
-  if (response.acknowledged) {
-    res.status(201).json(response.insertedId);
-  } else {
-    res.status(500).json(response.error || 'Error occurred while creating the trainer.');
+  try {
+    const trainer = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      specialty: req.body.specialty,
+      certification: req.body.certification
+    };
+    const response = await mongodb.getDb().db().collection('trainers').insertOne(trainer);
+    if (response.acknowledged) {
+      res.status(201).json(response.insertedId);
+    } else {
+      res.status(500).json('Error occurred while creating the trainer.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 const updateTrainer = async (req, res) => {
-  const trainerId = new ObjectId(req.params.id);
-  const trainer = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    specialty: req.body.specialty,
-    certification: req.body.certification
-  };
-  const response = await mongodb.getDb().db().collection('trainers').replaceOne({ _id: trainerId }, trainer);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Error occurred while updating the trainer.');
+  try {
+    const trainerId = new ObjectId(req.params.id);
+    const trainer = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      specialty: req.body.specialty,
+      certification: req.body.certification
+    };
+    const response = await mongodb.getDb().db().collection('trainers').replaceOne({ _id: trainerId }, trainer);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json('Error occurred while updating the trainer.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 const deleteTrainer = async (req, res) => {
-  const trainerId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('trainers').deleteOne({ _id: trainerId });
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Error occurred while deleting the trainer.');
+  try {
+    const trainerId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('trainers').deleteOne({ _id: trainerId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json('Error occurred while deleting the trainer.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
-module.exports = { 
-  getAll, 
-  getSingle, 
-  createTrainer, 
-  updateTrainer, 
-  deleteTrainer 
-};
+module.exports = { getAll, getSingle, createTrainer, updateTrainer, deleteTrainer };
--- gym-api/controllers/workouts.js
+++ gym-api/controllers/workouts.js
@@ -2,75 +2,89 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('workouts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const result = await mongodb.getDb().db().collection('workouts').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const workoutId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('workouts').find({ _id: workoutId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const workoutId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('workouts').find({ _id: workoutId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createWorkout = async (req, res) => {
-  const workout = {
-    exerciseName: req.body.exerciseName,
-    muscleGroup: req.body.muscleGroup,
-    sets: req.body.sets,
-    reps: req.body.reps,
-    weight: req.body.weight,
-    difficulty: req.body.difficulty,
-    restTime: req.body.restTime,
-    equipment: req.body.equipment
-  };
-  const response = await mongodb.getDb().db().collection('workouts').insertOne(workout);
-  if (response.acknowledged) {
-    res.status(201).json(response.insertedId);
-  } else {
-    res.status(500).json(response.error || 'Error occurred while creating the workout.');
+  try {
+    const workout = {
+      exerciseName: req.body.exerciseName,
+      muscleGroup: req.body.muscleGroup,
+      sets: req.body.sets,
+      reps: req.body.reps,
+      weight: req.body.weight,
+      difficulty: req.body.difficulty,
+      restTime: req.body.restTime,
+      equipment: req.body.equipment
+    };
+    const response = await mongodb.getDb().db().collection('workouts').insertOne(workout);
+    if (response.acknowledged) {
+      res.status(201).json(response.insertedId);
+    } else {
+      res.status(500).json('Error occurred while creating the workout.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 const updateWorkout = async (req, res) => {
-  const workoutId = new ObjectId(req.params.id);
-  const workout = {
-    exerciseName: req.body.exerciseName,
-    muscleGroup: req.body.muscleGroup,
-    sets: req.body.sets,
-    reps: req.body.reps,
-    weight: req.body.weight,
-    difficulty: req.body.difficulty,
-    restTime: req.body.restTime,
-    equipment: req.body.equipment
-  };
-  const response = await mongodb.getDb().db().collection('workouts').replaceOne({ _id: workoutId }, workout);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Error occurred while updating the workout.');
+  try {
+    const workoutId = new ObjectId(req.params.id);
+    const workout = {
+      exerciseName: req.body.exerciseName,
+      muscleGroup: req.body.muscleGroup,
+      sets: req.body.sets,
+      reps: req.body.reps,
+      weight: req.body.weight,
+      difficulty: req.body.difficulty,
+      restTime: req.body.restTime,
+      equipment: req.body.equipment
+    };
+    const response = await mongodb.getDb().db().collection('workouts').replaceOne({ _id: workoutId }, workout);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json('Error occurred while updating the workout.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 const deleteWorkout = async (req, res) => {
-  const workoutId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('workouts').deleteOne({ _id: workoutId });
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Error occurred while deleting the workout.');
+  try {
+    const workoutId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('workouts').deleteOne({ _id: workoutId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json('Error occurred while deleting the workout.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
-module.exports = { 
-  getAll, 
-  getSingle, 
-  createWorkout, 
-  updateWorkout, 
-  deleteWorkout 
-};
+module.exports = { getAll, getSingle, createWorkout, updateWorkout, deleteWorkout };
